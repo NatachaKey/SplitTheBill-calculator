3e2d3e3a85657da848cefb50a3c0699af587cf49
Glitch (glitch-hello-website) <none>
2022-07-02 16:35:06 +0000

    🌔💳 Checkpoint ./index.html:42900803/5895 ./style.css:42900803/5859 ./script.js:42900803/2039

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
+const button = document.querySelector("#btn");
+button.addEventListener("click", calculateAmount);
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+const buttonTip =document.querySelector("#addTip");
+buttonTip.addEventListener("click", showTip )
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
+
+function showTip(e){
+    e.preventDefault();
+    tip.style.display="block";
 }
 
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+
+function calculateAmount(e){
+    e.preventDefault();
+    const bill= document.querySelector("#bill").value;
+    const people= document.querySelector("#people").value;
+    const tip= document.querySelector("#tip").value;
+
+if (bill === "" || people ==="" || people<1 || bill <0) {
+    Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Please enter your information!',
+      })
+}
+
+let amountPerPerson= bill / people;
+let tipPerPerson= (bill * tip)/ people;
+let totalSum = amountPerPerson + tipPerPerson;
+
+amountPerPerson= amountPerPerson.toFixed(2);
+tipPerPerson= tipPerPerson.toFixed(2);
+totalSum=totalSum.toFixed(2);
+
+document.querySelector("#dividedBill").textContent=amountPerPerson;
+document.querySelector("#dividedTip").textContent= tipPerPerson;
+document.querySelector("#billAndTip").textContent =totalSum;
+
+}
+
+
