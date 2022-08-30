18771105436afd9fa3c840a54b54373bd3bb54c6
Glitch (glitch-hello-website) <none>
2022-08-30 16:09:28 +0000

    🍓😺 Checkpoint ./script.js:42900803/251 ./style.css:42900803/181

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-gsap.from("#container", {y:300, delay:0.2, duration:3, opacity:0.5, ease:"power4.out"})
-
+gsap.from("#container", {y:300, delay:0.4, duration:3, opacity:0, ease:"power4.out"})
+gsap.from("form", {x:-300, delay:0.6, duration:2, opacity:0, ease:"power4.out"})
+gsap.from("#showTotalAmountToPay", {x:300, delay:0.6, duration:2, opacity:0, ease:"power4.out"})
 
 const button = document.querySelector("#btn");
 button.addEventListener("click", calculateAmount);
